82a72d9aa323acfccbeb64969fa08b504a8b8c3c
anassazz <anass.azz2030@gmail.com>
2025-05-08 10:41:49 +0100

    feat: add mongoose integration and update environment configuration

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import cors from 'cors'
 import 'dotenv/config'
 import authRoutes from './routes/authRoutes.js'
 
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 5000
 
 app.use('/',authRoutes)
 
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log(' ✅ MongoDB Connected'))
+.catch((err)=>console.log("Database not connected",err)) 
 
 
 
